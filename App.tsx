fa11b2f1a080cb16f126a51ba017033f96199826
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-07-14 15:07:35 -0500

    Switch

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import AppTextInput from "./components/AppTextInput";
+import { StyleSheet, Switch } from "react-native";
 import Screen from "./components/Screen";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
-  console.log(firstName);
+  const [isNew, setIsNew] = useState(false);
+  console.log(isNew);
 
   return (
     <Screen>
-      <AppTextInput icon="email" textProps={{ placeholder: "email" }} />
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
     </Screen>
   );
 }
--- config/styles.ts
+++ config/styles.ts
@@ -9,3 +9,7 @@ export default {
     color: colors.dark,
   },
 };
+
+// In this snippet, we define the default styles for our app. The styles object has two properties:
+//    colors: This is an object that contains all the colors used in the app.
+//    text: This is an object that contains the default styles for text components in the app.
