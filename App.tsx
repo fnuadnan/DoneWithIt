37526d9bfd74314b72f4c19dabf4ac7680c2be22
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-07-15 13:59:18 -0500

    Build better forms with react hook form

--- App.tsx
+++ App.tsx
@@ -1,16 +1,72 @@
-import React, { useState } from "react";
-import { StyleSheet } from "react-native";
-import { Category } from "./entities";
-import LoginScreen from "./screens/LoginScreen";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Image, StyleSheet } from "react-native";
+import AppButton from "./components/AppButton";
+import AppTextInput from "./components/AppTextInput";
+import Screen from "./components/Screen";
 
 export default function App() {
-  const [category, setCategory] = useState<Category | undefined>(undefined);
-  return <LoginScreen />;
+  const { control, handleSubmit } = useForm();
+  return (
+    <Screen cssProp={styles.container}>
+      <Image style={styles.logo} source={require("./assets/logo-red.png")} />
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { onChange, value } }) => (
+          <AppTextInput
+            icon="email"
+            textProps={{
+              autoCapitalize: "none",
+              autoCorrect: false,
+              keyboardType: "email-address",
+              placeholder: "Email",
+              textContentType: "emailAddress",
+              onChangeText: onChange,
+              value: value,
+            }}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { onChange, value } }) => (
+          <AppTextInput
+            icon="lock"
+            textProps={{
+              autoCapitalize: "none",
+              autoCorrect: false,
+              placeholder: "Password",
+              secureTextEntry: true,
+              textContentType: "password",
+              onChangeText: onChange,
+              value: value,
+            }}
+          />
+        )}
+      />
+      <AppButton
+        title="Login"
+        onPress={handleSubmit((data) => console.log(data))}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#f8f4f4",
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  error: {
+    color: "red",
+    alignSelf: "center",
   },
 });
