f0f8ec1277e3612adfbc65b70c06d6cabd623305
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-07-15 15:49:56 -0500

    Validation with zod

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,56 +1,71 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, Text } from "react-native";
+import { z } from "zod";
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
 
-const LoginScreen = () => {
-  const { control, handleSubmit } = useForm();
+const schema = z.object({
+  email: z.string().email("Invalid email").min(5, "Email is too short"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
 
+const LoginScreen = () => {
+  const { control, handleSubmit, reset } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = (data: any) => {
+    console.log(data);
+    reset(); // Optionally reset the form after submission
+  };
   return (
     <Screen cssProp={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
       <Controller
         control={control}
         name="email"
-        render={({ field: { onChange, value } }) => (
-          <AppTextInput
-            icon="email"
-            textProps={{
-              autoCapitalize: "none",
-              autoCorrect: false,
-              keyboardType: "email-address",
-              placeholder: "Email",
-              textContentType: "emailAddress",
-              onChangeText: onChange,
-              value: value,
-            }}
-          />
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
+          <>
+            <AppTextInput
+              icon="email"
+              textProps={{
+                autoCapitalize: "none",
+                autoCorrect: false,
+                keyboardType: "email-address",
+                placeholder: "Email",
+                textContentType: "emailAddress",
+                onChangeText: onChange,
+                value: value,
+              }}
+            />
+            {error && <Text style={{ color: "red" }}>{error.message}</Text>}
+          </>
         )}
       />
       <Controller
         control={control}
         name="password"
-        render={({ field: { onChange, value } }) => (
-          <AppTextInput
-            icon="lock"
-            textProps={{
-              autoCapitalize: "none",
-              autoCorrect: false,
-              placeholder: "Password",
-              secureTextEntry: true,
-              textContentType: "password",
-              onChangeText: onChange,
-              value: value,
-            }}
-          />
+        render={({ field: { onChange, value }, fieldState: { error } }) => (
+          <>
+            <AppTextInput
+              icon="lock"
+              textProps={{
+                autoCapitalize: "none",
+                autoCorrect: false,
+                placeholder: "Password",
+                secureTextEntry: true,
+                textContentType: "password",
+                onChangeText: onChange,
+                value: value,
+              }}
+            />
+            {error && <Text style={{ color: "red" }}>{error.message}</Text>}
+          </>
         )}
       />
-      <AppButton
-        title="Login"
-        onPress={handleSubmit((data) => console.log(data))}
-      />
+      <AppButton title="Login" onPress={handleSubmit(onSubmit)} />
     </Screen>
   );
 };
