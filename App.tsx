576634fcd87638b46d44b2fb131e4a2c3d743724
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-07-25 20:57:46 -0500

    Accessing the library

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import * as ImagePicker from "expo-image-picker";
-import { useEffect } from "react";
-import { StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, Image, StyleSheet } from "react-native";
 import Screen from "./components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState<string | null>(null); // Image URI state
+
+  // Request permission to access the library
   const requestPermission = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!granted) {
@@ -15,7 +18,27 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  // Select image from the library
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+      3;
+    } catch (error) {
+      console.error("Error picking an image: ", error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      {imageUri && (
+        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      )}
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({});
