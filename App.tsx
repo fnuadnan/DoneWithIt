4b29c697833ecada6c5db3683936555912dcc97c
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-08-03 19:08:33 -0500

    Navigating between screens

--- App.tsx
+++ App.tsx
@@ -1,8 +1,44 @@
-import { StyleSheet } from "react-native";
-import WelcomeScreen from "./screens/WelcomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, StyleSheet, Text } from "react-native";
+import Screen from "./components/Screen";
+
+const Tweets = ({ navigation }: any) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
+      />
+    </Screen>
+  );
+};
+
+const TweetDetails = ({ route }: any) => {
+  return (
+    <Screen>
+      <Text>Tweet Details {route.params.id}</Text>
+    </Screen>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  return ;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
