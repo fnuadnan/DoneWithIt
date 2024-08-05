098d7af39770b685cad9f40aff8dd3127d66389d
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-08-04 19:51:54 -0500

    Navigation Theme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import AuthNavigator from "./navigation/AuthNavigator";
+import navigationTheme from "./navigation/navigationTheme";
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
