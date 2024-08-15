1317c627f1e80f9bc993c58945f081e51010ff58
Fnu Adnan <129648022+fnuadnan@users.noreply.github.com>
2024-08-14 20:47:01 -0400

    Caching successful request

--- App.tsx
+++ App.tsx
@@ -1,27 +1,14 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { Text } from "react-native";
-import Screen from "./components/Screen";
+import AppNavigator from "./navigation/AppNavigator";
+import navigationTheme from "./navigation/navigationTheme";
 
 const App = () => {
-  const storeData = async () => {
-    try {
-      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
-      const value = await AsyncStorage.getItem("person");
-
-      const keys = await AsyncStorage.getAllKeys();
-      const items = await AsyncStorage.multiGet(keys);
-      console.log(items);
-
-      console.log(value);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
-    <Screen>
-      <Text onPress={storeData}>Hello World</Text>
-    </Screen>
+    <NavigationContainer theme={navigationTheme}>
+      {/* <AuthNavigator /> */}
+      <AppNavigator />
+    </NavigationContainer>
   );
 };
 
--- services/api-clients.ts
+++ services/api-clients.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import cache from "../Utility/cache";
 
 const axiosInstance = axios.create({
   baseURL: "http://192.168.1.91:3000/api",
@@ -12,15 +13,29 @@ class APIClient<T> {
 
   // get by id
   async get(id: string) {
+    const cacheKey = this.endpoint + id; // create a cache key
+
     try {
-      const response = await axiosInstance.get<T>(`${this.endpoint}/${id}`);
-      return response.data;
+      // if successful, store the data in the cache and return the data
+      const response = await axiosInstance.get<T>(cacheKey); // get the data from the endpoint
+      if (response.data) {
+        await cache.store(cacheKey, response.data); // store the data in the cache
+        return response.data; // return the dat
+      }
+
+      return null;
     } catch (error) {
-      throw new Error("Error fetching data");
+      console.error("Network request failed, falling back to cache:", error);
+
+      // If the network request fails, try to get data from the cache
+      const cachedData = await cache.get<T>(cacheKey);
+      if (cachedData) return cachedData; // Return cached data if available
+
+      throw new Error("Error fetching data from both network and cache.");
     }
   }
 
-  // get all
+  // get all data from the endpoint if not get from cache
   async getAll() {
     try {
       const response = await axiosInstance.get<T[]>(this.endpoint);
